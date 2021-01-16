4d870eb14e2f10781304a438ad3ac518b9d25d92
Jani Ärväs <jarvas77@hotmail.com>
2021-01-16 16:05:20 +0200

    first commit

---
+++ Fibonacci/Controllers/FibonacciController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+
+namespace Fibonacci.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FibonacciController : ControllerBase
+    {
+
+        private readonly ILogger<FibonacciController> _logger;
+
+        // Without Postman Number 0 is used to set up interface, default value, returns "true"
+        private int Number = 0;
+ 
+
+        public FibonacciController(ILogger<FibonacciController> logger)
+        {
+            _logger = logger;
+        }
+
+        // Test function for get values for Postman and for startup
+        [HttpGet]
+        public bool GetFibonacci(int number)
+        {
+            Number = number;
+            return isFibonacci(Number);
+        }
+
+        // Actual interface for application UI, can be used also with Postman - "{\"number\":\"7\"}" - "raw, JSON"
+        [HttpPost]
+        public bool PostFibonacci([FromBody] dynamic data)
+        {
+            Object result = data;
+            string value = Convert.ToString(result);
+            dynamic stuff = JsonConvert.DeserializeObject(value);
+            String exactValue = stuff.number;
+            int number = Int32.Parse(exactValue);
+            return isFibonacci(number);
+        }
+
+        // A utility function that returns 
+        // true if x is perfect square 
+        static bool isPerfectSquare(int x)
+        {
+            int s = (int)Math.Sqrt(x);
+            return (s * s == x);
+        }
+
+        // Returns true if n is a  
+        // Fibonacci Number, else false 
+        static bool isFibonacci(int n)
+        {
+            // n is Fibonacci if one of 
+            // 5*n*n + 4 or 5*n*n - 4 or  
+            // both are a perfect square 
+            return isPerfectSquare(5 * n * n + 4) ||
+                   isPerfectSquare(5 * n * n - 4);
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace Fibonacci
+{
+    public class Fibonacci
+    {
+
+        public int Number { get; set; }
+    }
+
+}
